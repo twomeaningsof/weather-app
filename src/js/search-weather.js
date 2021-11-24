60be71623af57fb9aca7ae76557865d3c26558d3
unknown <twomeaningsof@gmail.com>
2021-11-24 23:59:21 +0100

    Revamp the code Also apply different method of filtering data in terms of days

--- src/js/search-weather.js
+++ src/js/search-weather.js
@@ -1,55 +1,78 @@
 const apiKey = "56316389de29eb42a940d374da953aa8";
+const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast'
 const searchElement = document.getElementById('search');
 
-const updatePage = (forecast) => {
-  document.getElementById('header-cloud').innerText = `${forecast[0][1]}%`;
-  document.getElementById('header-wind').innerText = `${Math.round(forecast[0][2])}m/s`;
-  document.getElementById('header-humidity').innerText = `${forecast[0][3]}%`;
-  document.getElementById('main-city').innerText = forecast[0][4];
-  document.getElementById('main-status').innerText = forecast[0][5];
-  document.getElementById('main-temperature').innerText = `${Math.round(forecast[0][0])}°`;
-  document.getElementById('tmw-temperature').innerText = `${Math.round(forecast[1][0])}°`;
-  document.getElementById('tmw-cloud').innerText = `${forecast[1][1]}%`;
-  document.getElementById('tmw-wind').innerText = `${Math.round(forecast[1][2])}m/s`;
-  document.getElementById('tmw-humidity').innerText = `${forecast[1][3]}%`;
-  document.getElementById('2-days-temperature').innerText = `${Math.round(forecast[2][0])}°`;
-  document.getElementById('2-days-cloud').innerText = `${forecast[2][1]}%`;
-  document.getElementById('2-days-wind').innerText = `${Math.round(forecast[2][2])}m/s`;
-  document.getElementById('2-days-humidity').innerText = `${forecast[2][3]}%`;
-  document.getElementById('3-days-temperature').innerText = `${Math.round(forecast[3][0])}°`;
-  document.getElementById('3-days-cloud').innerText = `${forecast[3][1]}%`;
-  document.getElementById('3-days-wind').innerText = `${Math.round(forecast[3][2])}m/s`;
-  document.getElementById('3-days-humidity').innerText = `${forecast[3][3]}%`;
+const setTemperature = (value) => `${Math.round(value)}°`;
+const setCloudiness = (value) => `${value}%`;
+const setWind = (value) => `${Math.round(value)}m/s`;
+const setHumidity = (value) => `${value}%`;
+
+const getUrl = () => {
+  const city = document.getElementById('city').value;
+  return `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
 }
 
-const fetchData = async (url) => {
+const fetchData = async () => {
+  const url = getUrl();
+
   try {
     const res = await fetch(url);
     const data = await res.json();
-    let forecast = [];
-    for (let i = 0; i<25; i += 8) {
-        const temperature = data.list[i].main.temp;
-        const cloudiness = data.list[i].clouds.all;
-        const wind = data.list[i].wind.speed;
-        const humidity = data.list[i].main.humidity;
-        const city = data.city.name;
-        const status = data.list[i].weather[0].description;
-        forecast.push([temperature, cloudiness, wind, humidity, city, status]);
-      }
-    return updatePage(forecast);
+    console.log(data);
+    return data;
   } catch (error) {
     return alert(`Something went wrong. ${error}`)
   }
 }
 
+const filterData = (data) => [[data.list[0],data.list[8],data.list[16],data.list[24]],data.city.name]
 
-const getUrl = (e) => {
-  e.preventDefault();
-  let city = document.getElementById('city').value;
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  searchElement.reset();
-  console.log(`getUrl: ${url}`);
-  return fetchData(url);
+const mapDataToForecast = (data) => data[0].map(listItem => ({
+  temperature: listItem.main.temp,
+  cloudiness: listItem.clouds.all,
+  wind: listItem.wind.speed,
+  humidity: listItem.main.humidity,
+  status: listItem.weather[0].description,
+  city: data[1]
+}))
+
+const updatePage = (forecast) => {
+  const today = forecast[0];
+  const tomorrow = forecast[1];
+  const inTwoDays = forecast[2];
+  const inThreeDays = forecast[3];
+
+  document.getElementById('main-city').innerText = today.city;
+  document.getElementById('main-status').innerText = today.status;
+
+  document.getElementById('main-temperature').innerText = setTemperature(today.temperature)
+  document.getElementById('tmw-temperature').innerText = setTemperature(tomorrow.temperature);
+  document.getElementById('2-days-temperature').innerText = setTemperature(inTwoDays.temperature);
+  document.getElementById('3-days-temperature').innerText = setTemperature(inThreeDays.temperature);
+
+  document.getElementById('header-cloud').innerText = setCloudiness(today.cloudiness);
+  document.getElementById('tmw-cloud').innerText = setCloudiness(tomorrow.cloudiness);
+  document.getElementById('2-days-cloud').innerText = setCloudiness(inTwoDays.cloudiness);
+  document.getElementById('3-days-cloud').innerText = setCloudiness(inThreeDays.cloudiness);
+
+  document.getElementById('header-wind').innerText = setWind(today.wind);
+  document.getElementById('tmw-wind').innerText = setWind(tomorrow.wind);
+  document.getElementById('2-days-wind').innerText = setWind(inTwoDays.wind);
+  document.getElementById('3-days-wind').innerText = setWind(inThreeDays.wind);
+
+  document.getElementById('header-humidity').innerText = setHumidity(today.humidity)
+  document.getElementById('tmw-humidity').innerText = setHumidity(tomorrow.humidity)
+  document.getElementById('2-days-humidity').innerText = setHumidity(inTwoDays.humidity)
+  document.getElementById('3-days-humidity').innerText = setHumidity(inThreeDays.humidity)
 }
 
-searchElement.addEventListener('submit', getUrl);
+const handleSearch = async (event) => {
+  event.preventDefault();
+  const data = await fetchData();
+  const filteredData = filterData(data);
+  const forecast = mapDataToForecast(filteredData);
+  updatePage(forecast);
+  return searchElement.reset();
+}
+
+searchElement.addEventListener('submit', handleSearch);
