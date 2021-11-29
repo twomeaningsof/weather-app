c81f4f1fdb5673d72e0d4e48aed7b10d5ff65171
unknown <twomeaningsof@gmail.com>
2021-11-29 22:52:32 +0100

    Add background change on time of the day

--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -1,4 +1,4 @@
-import { fetchData, filterList, mapListToForecast, updatePage } from "./utils.js";
+import { fetchData, changeBackground, filterList, mapListToForecast, updatePage } from "./utils.js";
 import { apiKey, baseUrl } from "./constants.js";
 
 const getGeolocationUrl = (latitude, longitude) =>
@@ -9,6 +9,7 @@ const getGeolocationError = () => alert(`Unable to retrieve your location.`);
 const handleSetDataFromGeolocation = async (position) => {
   const { latitude, longitude } = position.coords;
   const { list, city } = await fetchData(getGeolocationUrl(latitude, longitude));
+  changeBackground(list[0].dt, city.timezone, city.sunrise, city.sunset);
   const filteredList = filterList(list);
   const forecast = mapListToForecast(filteredList);
   updatePage({ forecast, city: city.name });
