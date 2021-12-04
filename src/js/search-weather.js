9526167b22069607c6fb63ac59778bcd9023826f
unknown <twomeaningsof@gmail.com>
2021-12-05 00:48:32 +0100

    Fix submit and blur events to work along Additionally, stop handleSearch function once the fetch response cod is 404.

--- src/js/search-weather.js
+++ src/js/search-weather.js
@@ -5,23 +5,29 @@ const searchElement = document.getElementById("search");
 const searchTextElement = document.getElementById("city");
 
 const handleSearch = async (event) => {
-  event.preventDefault();
-  localStorage.removeItem("city");
+  if (searchTextElement.value !== "") {
+    event.preventDefault();
+    localStorage.removeItem("city");
 
-  const {
-    list,
-    city: { timezone, sunrise, sunset, name: city },
-  } = await fetchData(getUrl());
-  const [{ dt: currentTime }] = list;
-  const filteredList = filterList(list);
-  const forecast = mapListToForecast(filteredList);
+    const fetchedData = await fetchData(getUrl());
+    if (fetchedData.cod !== "404") {
+      const {
+        list,
+        city: { timezone, sunrise, sunset, name: city },
+      } = fetchedData;
+      const [{ dt: currentTime }] = list;
+      const filteredList = filterList(list);
+      const forecast = mapListToForecast(filteredList);
 
-  localStorage.setItem("city", city);
-  updatePage({ forecast, city });
-  changeBackground(currentTime, timezone, sunrise, sunset);
-  searchElement.reset();
-  goToTopOfPage();
-  return searchTextElement.blur();
+      localStorage.setItem("city", city);
+
+      updatePage({ forecast, city });
+      changeBackground(currentTime, timezone, sunrise, sunset);
+      searchElement.reset();
+      goToTopOfPage();
+      return searchTextElement.blur();
+    }
+  }
 };
 
 const getUrl = () => {
